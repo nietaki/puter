52ee8662431d118cce63ac892b5f8585e37947f1
Jacek Królikowski <nietaki@gmail.com>
2023-06-19 16:13:12 +0200

    Add nginx, update osx setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,18 @@
 set -e # Exit if any fail
 set -o pipefail
 
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+  # Do something under Mac OS X platform        
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  # Do something under GNU/Linux platform
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+  # Do something under 32 bits Windows NT platform
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+  # Do something under 64 bits Windows NT platform
+fi
+
 if [[ ! -f ~/.ssh/id_rsa.pub ]]; then
   echo "ssh keys don't exist, will generate..."
   read -r -s -p "Password: " password
